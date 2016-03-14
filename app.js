2d62164ac9980db6770efb9ff39a7b52ef02047f
Reme Le Hane <remelh@globalkinetic.com>
2016-03-14 15:29:27 +0200

    Code tidy

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var express     = require('express'),
     logger      = require('morgan'),
     bodyParser  = require('body-parser'),
     routes      = require('./routes/index'),
-    users       = require('./routes/users'),
     app         = express();
 
 // view engine setup
@@ -18,11 +17,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+app.use((req, res, next) => {
+  let err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
@@ -32,7 +30,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+  app.use((err, req, res, next) => {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
@@ -43,7 +41,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
